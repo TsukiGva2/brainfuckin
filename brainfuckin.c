6f21054109ea161865584749dbfd9fc12b5bc9f9
Rodrigo <dreamviolent56@gmail.com>
2021-03-16 22:59:43 -0300

    fixed [] bug and added some comments

--- brainfuckin.c
+++ brainfuckin.c
@@ -1,60 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define min(a,b) (a<b?a:b)
 #define cell_max 1048
 #define buf_size 1048
 
+#define skip 0
+#define eval 1
+
 int main (void) {
-	unsigned char *cells = (char*)malloc(cell_max); // qual o ponto de colocar aquele + 1? tirei fodase 😎
-	int cptr = 0;                                   // tipo o cursor sla
-	char buf[buf_size];                             // lixo
+	unsigned char *cells = (char*)malloc(cell_max);
+	int cptr = 0; // memory pointer
+	char buf[buf_size]; // input buffer
     
+	int state = eval;
 
-	char* stack[10];                                // bagulho de memoria
-	int curr = 0;                                   // coisa de brainfuck tomanocu
+	char* stack[10]; // loop stack
+	int curr = 0; // current position on stack
+	bool ignore = false;
 
-	if (cells == NULL) exit(1);                     // se nao tem cells sai
+	if (cells == NULL) exit(1); // exit if malloc error
+	
+	memset(cells, 0, cell_max); // zero out all cells
 
-	memset(cells, 0, cell_max);                     // faz o array das cells receber 0 em todos os indices?
-	memset(buf, 0, buf_size);                       // faz o array do buf receber 0 em todos os indices?
+	while (fgets(buf, buf_size, stdin)) { // get a string from stdin to buf
 
-	while (fgets(buf, buf_size, stdin)) {           // de algum jeito fica pegando o input aí
+        char* pc = buf; // program counter
 
-        char* pc = buf;                             // ponteiro que aponta pra string que o user colocou
-
-		while (*pc != '\0' && *pc != '\n') {        // enquanto essa string nao ternminar ou o cara apertar enter
+		while (*pc != '\0' && *pc != '\n') { // while ! end of string and ! EOL
 			
+			if (state != skip || *pc == ']' || *pc == '[') {
+
 			switch (*pc) {
-				case '+':                           
-					*(cells+cptr) += 1;             // adiciona 1 a célula na posição cptr
+				case '+':
+					*(cells+cptr) += 1; // increment cell
 				    break;
 
 				case '-':
-					*(cells+cptr) -= 1;             // faz o contrario
+					*(cells+cptr) -= 1; // decrement cell
 				    break;
                     
 				case '>':							
-					if (cptr<cell_max) {			// só faz isso se nao ultrapassar o valor maximo de celulas
-						cptr++;						// aqui bota o "cursor" pra frente (cursor = qual celula tá)
+					if (cptr<cell_max) { // if on cell range
+						cptr++; // increment memory pointer
 					}
-					else {							// se ultrapassar acabou a memoria haha lol
+					else { // die if not on cell range (currently 1048)
 						printf("out of memory, exiting\n");
 						exit(1);
 					}
 				    break;
-				case '<':							// aqui faz o contrario da parada
-					if (cptr>0) {					// aparentemente ele nao pode ser um valor negativo huh
-						cptr--;	
+				case '<':
+					if (cptr>0) { // prevent memory pointer from being < 0 and giving us a delightful segfault
+						cptr--; // decrement memory pointer
 					}
 				    break;
-				case '[':							// nao sei o que isso faz pq eu nao sei brainfuck e nem quero saber (na vdd eu sabia só nao lembro de mais nada pq faz muito tempo que eu vi)
+				case '[': // start loop
+					if (state == skip) {
+						ignore = true;
+						break;
+					}
 					if (curr<10) {
-						if (cells[cptr] != 0) {
-							curr += 1;
-							stack[curr] = pc;
-						}
+						curr += 1; // increment "stack" position
+						stack[curr] = pc; // "pushing" program counter to "stack"
+						state = skip;
 					}
 					else {
 						printf("stack overflow error, exiting\n");
@@ -62,6 +72,10 @@ int main (void) {
 					}
 				    break;
 				case ']':
+					if (ignore) {
+						ignore = false;
+						break;
+					}
 					if (curr>0) {
 						if (cells[cptr] != 0) {
 							pc = stack[curr];
@@ -69,33 +83,32 @@ int main (void) {
 						else {
 							curr--;
 						}
+						state = eval;
 					}
 					else {
 						printf("misplaced ]\n");
 						exit(1);
 					}
-				    break;
+				break;
+				case '.':
+					putchar(cells[cptr]);
+				break;
+			}
 			}
 
-			//printf("read: %c, current cell's value: %d on position: %d\n", 
-			//		*pc, (unsigned int)cells[cptr], cptr);
+			//printf("\nread: %c, current cell's value: %d on position: %d, state = %d\n", 
+			//		*pc, (unsigned int)cells[cptr], cptr, state);
 			
-			cptr = min(cptr,cell_max);									// acho isso redundante, nao muda nada no programa (eu acho né) (mesmo que o ponteiro passe da ultima célula, o programa vai ser finalizado mesmo assim) (pelo menos eu só testei os >>>> e os ++++ ----- +++-+--+ -+-+ ><><><><>, não cheguei a testar os [] [] [[[[ [][] [] ][]]]], nem sei pra que serve lololololololol, eta porra comentario gigante slc blz parei)
-
-			// printa os bagulho
-			printf("cells: ");
-			for (int i = min(0,cptr-2); i < cptr+3; i++)
+			// print stuff
+			printf("\ncells: ");
+			for (int i = min(0,cptr-2); i < min(cptr+3,cell_max); i++)
 				printf("%d ",cells[i]);
 
 			putchar('\n');
 
 			pc++;
 		}
-		memset(buf, 0, buf_size);										// tbm acho isso aqui desnecessario (ao meu ver, nao mudou nada no programa)
 	}
-
+	free(cells);
+	return 0;
 }
-
-/*
-	Reject humanity, become monke 🐒
-*/
